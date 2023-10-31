3bacbf4e0bf689887ca59fcb33aae06406dfbbde
Henri Heyden <henri@heyden5.de>
2023-10-31 21:10:41 +0100

    Ass 1.32 should work, there seem to be issues with the automatic review

---
+++ Week 1/Ass1.32.c
@@ -0,0 +1,21 @@
+#include <stdio.h> // changed
+
+// f is meant to initialise an as pointer declared array
+void f (int **ptr) { // changed
+    int ar[] = {1,2,3,4};
+    *ptr = ar; // changed
+}
+
+int main() {
+    int *arr;
+    f(&arr); // changed
+    // Print all the elements of the array 
+    for(int i=0; i < 4; i++) { //changed
+        int d = arr[i];
+        printf("The %d-th element is %d.\n", i, d); // changed
+    } //changed
+    return 1;
+}
+
+
+
