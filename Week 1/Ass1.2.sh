e63575bceeee6a9f44092c23a8d5f36f24853496
Henri Heyden <henri@heyden5.de>
2023-10-31 19:58:27 +0100

    Ass1.2.sh finished

---
+++ Week 1/Ass1.2.sh
@@ -0,0 +1,11 @@
+mkdir directory0
+cd directory0
+echo "Hello World!" >> text.txt
+cat text.txt
+mkdir directory1
+mkdir directory1/directory2
+touch test
+touch E
+mv test ./directory1/directory2/
+mv E ./directory1/directory2/
+rm ./directory1/directory2/E
