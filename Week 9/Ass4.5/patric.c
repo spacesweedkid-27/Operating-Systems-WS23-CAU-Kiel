f46ba99a656df7ab8698664c519e2854560d6930
Henri Heyden <henri@heyden5.de>
2024-01-04 18:26:04 +0100

    OK, now everything works, there are still some small things I have to fix, but this should be easy. Won't work on it today though.

--- Week 9/Ass4.5/patric.c
+++ Week 9/Ass4.5/patric.c
@@ -16,9 +16,14 @@ struct state {
   int active, finished;
 } state;
 
-short should_output = 0;
-
 sem_t semaphore;
+sem_t output_semaphore;
+
+// simple printer, prints the triangle as it should be parsed.
+void print_triangle(struct triangle input){
+  printf("(%d,%d),(%d,%d),(%d,%d)\n", input.point[0].x, input.point[0].y, input.point[1].x, input.point[1].y, input.point[2].x, input.point[2].y);
+}
+
 
 /**
  * @brief Callback function for the countPoints() function of triangle.h.
@@ -30,12 +35,12 @@ sem_t semaphore;
 static void calc_finished_cb(int boundary, int interior){
   // ENTERING CRITICAL AREA
   sem_wait(&semaphore);
-  state.boundary += boundary;
-  state.interior += interior;
-  state.active--;
-  state.finished++;
+  if (boundary + interior != 0){
+    state.boundary += boundary;
+    state.interior += interior;
+    sem_post(&output_semaphore);
+  }
 
-  should_output = 1;
   sem_post(&semaphore);
   // EXITING CRITICAL AREA
 }
@@ -50,7 +55,21 @@ static void calc_finished_cb(int boundary, int interior){
 static void *worker(void *param){
   // cast input back to right format
   struct triangle* input = (struct triangle*) param;
+  // check
+  print_triangle(*input);
   countPoints(input, &calc_finished_cb);
+
+  // ENTERING CRITICAL AREA
+  sem_wait(&semaphore);
+  state.active--;
+  state.finished++;
+  // Because we have another change, notify.
+  // sem_post(&output_semaphore);
+  sem_post(&semaphore);
+  // EXITING CRITICAL AREA
+
+  // lol, because the thread is detached, we don't have to free
+
   return NULL;
 }
 
@@ -60,13 +79,9 @@ static void *worker(void *param){
 **/
 static void *printer(void *param) {
   for (; ;){
-    while (!should_output)
-      ; // wait for output req
-    // ENTERING CRITICAL AREA
-    sem_wait(&semaphore);
-    should_output = 0;
-    sem_post(&semaphore);
-    // EXITING CRITICAL AREA
+    // Passive waiting using semaphores
+    sem_wait(&output_semaphore);
+
     printf("Found %d boundary and %d interior points, %d active threads, %d finished threads\n", state.boundary, state.interior, state.active, state.finished);
   }
   return NULL;
@@ -81,21 +96,18 @@ struct triangle* parse(char* input){
   struct triangle* temp = malloc(sizeof(struct triangle));
   // this looks so stupid, give a 24 char buffer so that if somebody has a valid input but spills their food on the keyboard just after that, the input is discarded.
   // passing an input too long confusingly gives the error: free(): double free detected in tcache 2, and aborts.
-  char should_be_empty[24]; // non-heap object, does not have to be freed!
+  char* should_be_empty = malloc(24);
   // this may or may not be the most cursed line I have written ever:
   // scan the input for the formula below, and check if sscanf could parse 6 arguments, which are the 6 decimals, if it could parse 7, then the string after the last ')' exists, which
   // makes the input unacceptable. There is no possiblilty for the case that we parsed 5 arguments correctly and pass a wrong string, because of the brackets seperating the last decimal and the string.
-  if (sscanf(input, "(%d,%d),(%d,%d),(%d,%d)%s\n", &temp->point[0].x, &temp->point[0].y, &temp->point[1].x, &temp->point[1].y, &temp->point[2].x, &temp->point[2].y, &should_be_empty) != 6) {
+  if (sscanf(input, "(%d,%d),(%d,%d),(%d,%d)%s\n", &temp->point[0].x, &temp->point[0].y, &temp->point[1].x, &temp->point[1].y, &temp->point[2].x, &temp->point[2].y, should_be_empty) != 6) {
+    free(should_be_empty);
     // return the nullpointer
     return NULL;
   }
 
+  free(should_be_empty);
   return temp;
-}
-
-// simple printer, prints the triangle as it should be parsed.
-void print_triangle(struct triangle input){
-  printf("(%d,%d),(%d,%d),(%d,%d)\n", input.point[0].x, input.point[0].y, input.point[1].x, input.point[1].y, input.point[2].x, input.point[2].y);
 }
 
 
@@ -103,20 +115,28 @@ int main(int argc, char *argv[]) {
   // dec THREAD_NUM
   int THREAD_NUM;
   // buffer to detect wrong args
-  char should_be_empty[24];
+  char* should_be_empty = malloc(24);
 
   if (argc != 2){
-    printf("Wrong amount of arguments.\n");
+    fprintf(stderr, "Wrong amount of arguments.\n");
     return 1;
   }
   // check if the string exists, then display error and exit otherwise init THREAD_NUM
-  if (sscanf(argv[1], "%d%s", &THREAD_NUM, &should_be_empty) != 1){
-    printf("Garbage arguments detected.\n");
+  if (sscanf(argv[1], "%d%s", &THREAD_NUM, should_be_empty) != 1){
+    fprintf(stderr, "Garbage arguments detected.\n");
     return 2;
+  }
+
+  if (THREAD_NUM < 1){
+    fprintf(stderr, "Please specify a thread number bigger than 0\n");
+    return 3;
   }
 
   // Init semaphore for threads with maximum access being 1.
   sem_init(&semaphore, 0, 1);
+
+  // Init semaphore for passive waiting in output_thread
+  sem_init(&output_semaphore, 0, 0);
 
   // Init output thread
   pthread_t output_thread;
@@ -133,6 +153,7 @@ int main(int argc, char *argv[]) {
     
     // Parse it.
     struct triangle* next = parse(line_buf);
+    fflush(stdin);
     // Check for wrong input.
     if (next){
       printf("Parse completed, result is: ");
@@ -149,6 +170,7 @@ int main(int argc, char *argv[]) {
       // Wait
     }
 
+
     // ENTERING CRITICAL AREA
     sem_wait(&semaphore);
     state.active++;
@@ -161,6 +183,8 @@ int main(int argc, char *argv[]) {
     pthread_create(&thread, NULL, &worker, next);
     // and detach it. // TODO: CHECK FOR ERRORS
     pthread_detach(thread);
+
+    //free(next);
     }
 
   // lol, I accidentally put the free in the while loop and wondered why I got a double free about before I could see "Parse completed", IO is slow...
