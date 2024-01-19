341cfec2c726d74276be9d33d0f3cc54313d259a
Henri Heyden <henri@heyden5.de>
2024-01-19 14:36:15 +0100

    10 Points added.

--- Week 11/Ass5.3.c
+++ Week 11/Ass5.3.c
@@ -4,18 +4,24 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <errno.h>
 
-enum {READ=0, WRITE=1}
+enum {READ=0, WRITE=1};
 
 void piping(int argc, char* argv[]);
 
-static int pip[2];
-
 int main(int argc, char* argv[]) {
+    // I hope this does not break anything
+    // (he doesn't know)
+    argv = &argv[1];
+    argc--;
+
+    // REMEMBER: '|' is left-associative.
+    
+
     piping(argc, argv);
     return 0;
 }
-
 
 /**
  * @brief Things(tm) went terrible wrong, print an error using a passed message 
@@ -23,11 +29,75 @@ int main(int argc, char* argv[]) {
  * 
  * @param msg the message (as a string) that is used as a prefix in our perror call
  */
+/*
 static void die(char *msg) {
   perror(msg);
   exit(EXIT_FAILURE);
 }
+*/
 
+int pipe_helper(int argc, char* argv[], int input_pipe[2]){
+  if(argc == 0){
+    return 0;
+  }
+
+  int output_pipe[2];
+  if(pipe(output_pipe))
+      perror("Outcoming pipe creation has failed");
+
+  int next_input_pipe[2];
+  if(pipe(next_input_pipe))
+      perror("Next incoming pipe creation has failed");
+
+  if(errno){
+      printf("Exiting.\n");
+      exit(1);
+  }
+
+  pid_t pid = fork();
+  // fork
+  if (pid > 0){
+    // parent
+    pid_t second_child = fork();
+    if (second_child > 0){
+      close(input_pipe[WRITE]);
+      dup2(input_pipe[READ], 0);
+      close(input_pipe[READ]);
+
+      close(output_pipe[READ]);
+      dup2(output_pipe[WRITE], 1);
+      close(output_pipe[WRITE]);
+    } else if (second_child == 0){
+      waitpid(second_child, NULL, 0);
+    } else {
+      perror("Fork failed");
+    }
+
+    execlp(argv[0], argv[0], NULL);
+  } else if (pid == 0){
+    // child
+    close(output_pipe[WRITE]);
+    dup2(output_pipe[READ], 0);
+    close(output_pipe[READ]);
+
+    if (argc > 1){
+      close(next_input_pipe[READ]);
+      dup2(next_input_pipe[WRITE], 1);
+      close(next_input_pipe[WRITE]);
+    }
+
+    execlp(argv[1], argv[1], NULL);
+  } else {
+    perror("Fork failed");
+  }
+
+  printf("Sucess\n");
+
+  argv = &argv[1];
+  argc--;
+
+  return pipe_helper(argc, argv, next_input_pipe);
+  }
 
 /**
  * @brief Function replicating the behaviour of the pipe command |
@@ -39,10 +109,20 @@ static void die(char *msg) {
  * @param argv 
  */
 void piping(int argc,  char* argv[]){
-    // Parse commands and check for errors in them.
-    int command_number = argc - 2;
+    if (argc < 2){
+      printf("Less than 2 arguments passed.\n");
+      exit(1);
+    }
 
-    // REMEMBER: '|' is left-associative.
-    
+    // create the first input pipe
+    int input_pipe[2];
 
+    if(pipe(input_pipe))
+      perror("Incoming pipe creation has failed");
+
+    // write "" into it
+    write(input_pipe[WRITE], "", 1);
+
+    // pass it to a helper
+    pipe_helper(argc, argv, input_pipe);
 }
